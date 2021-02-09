595b1857f4cd716ffbbb249001d67eb4bacab5c7
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-02-09 23:08:06 +0900

    userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,14 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{8,128}+\z/i }
 
   with_options presence: true do
-    validates :nickname
-    validates :birth_date
-  end
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
-    validates :first_name
-    validates :last_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :first_name_kana
-    validates :last_name_kana
+    validates :nickname, :birth_date
+  
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+      validates :first_name, :last_name
+    end
+  
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana, :last_name_kana
+    end
   end
 end
