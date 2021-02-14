51d80fa2aa13e6053c057ba7aa8948ee9531d581
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-02-14 18:18:00 +0900

    editアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:info,:category_id,:sales_status_id,:shipping_fee_status_id,
