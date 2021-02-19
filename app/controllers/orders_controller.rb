6e406d8553e8aa818f9e1fb2f7281572956e42c8
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-02-20 02:16:23 +0900

    ordersコントローラー記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    @item_transaction_address = ItemTransactionAddress.new
+  end
+
+  def create
+    @item_transaction_address = ItemTransactionAddress.new(transaction_params)
+    if @item_transaction_address.valid?
+      @item_transaction_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:item_transaction_address).permit(:token,:postal_code,:prefecture_id,:city,:address,:building,:phone_number).merge(item_id: item_id, user_id: current_user.id)
+  end
 end
