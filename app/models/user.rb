0ec7259acbdf21dd7d5dd63d374a71aa0cbdbd4a
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-02-20 01:03:37 +0900

    アソシエーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items, dependent: :destroy
+  has_many :item_transactions
   
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{8,128}+\z/i }
 
