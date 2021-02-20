dd547065ec644756294bbadd453dd0bac01b9184
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-02-21 03:34:24 +0900

    formオブジェクト

--- app/models/item_transaction_address.rb
+++ app/models/item_transaction_address.rb
@@ -1,9 +1,9 @@
 class ItemTransactionAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_transaction_id
+  attr_accessor :token, :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
-    validates :item_id, :user_id, :item_transaction_id
+    validates :item_id, :user_id, :token
     validates :prefecture_id, numericality: {other_than: 1}
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :phone_number, numericality: {only_integer: true}, length: { maximum: 11 }
@@ -12,6 +12,6 @@ class ItemTransactionAddress
 
   def save
     item_transaction = ItemTransaction.create(item_id: item_id, user_id: user_id)    
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, building: building, phone_number: phone_number, item_transaction_id: item_transaction.id)
+    Address.create(postal_code: postal_code,prefecture_id: prefecture_id,city: city,address: address,building: building,phone_number: phone_number,item_transaction_id: item_transaction.id)
   end
 end
